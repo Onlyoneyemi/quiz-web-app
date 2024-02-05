4c2d1f10e0c047e050893a58c3f8faf9a5d55fb9
Onlyoneyemi <bellopeyemie@gmail.com>
2024-02-05 18:44:01 +0000

    2024-02-05

--- script.js
+++ script.js
@@ -40,3 +40,4 @@ const questions = [
 const questionElement = document.getElementById("question")
 const answerButton = document.getElementById("answer-buttons")
 const nextButton = document.getElementById("next-btn")
+
